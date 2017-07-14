4267a493ae73dd51242a9767fa13a750fc16b6f6
Vitaliy Orlov <orlov0562@gmail.com>
2017-07-14 11:46:02 +0300

    Initial commit

---
+++ whois.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo "======== WELCOME TO WHOIS INTERACTIVE ========"
+echo "(c) Vitaliy Orlov, 2017 | orlov0562.github.com"
+echo "----------------------------------------------"
+echo "Enter domain name or q for exit:"
+while true; do
+
+    read DOMAIN
+    if [ "$DOMAIN" == "q" ] || [ "$DOMAIN" == "Q" ]; then
+	break
+    else
+	clear
+        whois "$DOMAIN"
+	echo "---------------------------------------------"
+	echo "Enter domain name or q for exit:"
+    fi
+
+done
